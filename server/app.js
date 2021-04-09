5ba50dbac5fbafe47b547a6923a1f415b27a8e0a
ces9960 <ces9960@rit.edu>
2021-04-09 02:58:07 -0400

    Redid A and it actually works this time

--- server/app.js
+++ server/app.js
@@ -6,51 +6,43 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
-const session = require('express-session');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 
-
 const mongooseOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-};
+    useNewUrlParser: true,
+    useUnifiedTypology: true,
+    useCreateIndex: true,
+}
 
-mongoose.connect(dbURL, mongooseOptions, (err) => {
-  if (err) {
-    console.log('Could not connect to database');
-    throw err;
-  }
+mongoose.connect(dbURL,mongooseOptions,(err) => {
+    if(err){
+        console.log('Could not connect to database');
+        throw err;
+    }
 });
 
 const router = require('./router.js');
 
 const app = express();
-app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+app.use('/assets',express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({
-  extended: true,
+    extended:true,
 }));
-app.use(session({
-  key: 'sessionid',
-  secret: 'Domo Arigato',
-  resave: true,
-  saveUninitialized: true,
-}));
-app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-app.set('views', `${__dirname}/../views`);
+app.engine('handlebars',expressHandlebars({defaultLayout:'main'}));
+app.set('view engine','handlebars');
+app.set('views',`${__dirname}/../views`);
 app.use(cookieParser());
 
 router(app);
 
-app.listen(port, (err) => {
-  if (err) {
-    throw err;
-  }
-  console.log(`Listening on port ${port}`);
+app.listen(port,(err) => {
+    if(err){
+        throw err;
+    }
+    console.log(`Listening on port ${port}`);
 });
--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -1,84 +1,80 @@
+const { response } = require('express');
+const { restArgs } = require('underscore');
 const models = require('../models');
 
-const { Account } = models;
+const Account = models.Account;
 
-
-const loginPage = (req, res) => {
-  res.render('login');
+const loginPage = (req,res) => {
+    res.render('login');
 };
 
-const signupPage = (req, res) => {
-  res.render('signup');
+const signupPage = (req,res) => {
+    res.render('signup');
 };
 
-const logout = (req, res) => {
-  req.session.destroy();
-  res.redirect('/');
+const logout = (req,res) => {
+    res.redirect('/');
 };
 
-const login = (request, response) => {
-  const req = request;
-  const res = response;
+const login = (request,response) => {
+    const req = request;
+    const res = response;
 
-  const username = `${req.body.username}`;
-  const password = `${req.body.password}`;
+    const username = `${req.body.username}`;
+    const password = `${req.body.pass}`;
 
-  if (!username || !password) {
-    return res.status(400).json({ error: 'RAWR! All fields are required' });
-  }
-
-  // account (lowercase) in authenticate() is always undefined for some reason
-  return Account.AccountModel.authenticate(username, password, (err, account) => {
-    if (err || !account) {
-      return res.status(401).json({ error: 'Wrong username or password' });
+    if(!username || !password){
+        return res.status(400).json({error:'RAWR! All fields are required'});
     }
-    req.session.account = Account.AccountModel.toAPI(account);
-    return res.json({ redirect: '/maker' });
-  });
+
+    return Account.AccountModel.authenticate(username,password,(err,account) => {
+        if(err || !account){
+            return res.status(401).json({error: 'Wrong username or password'});
+        }
+
+        return res.json({redirect:'/maker'});
+    });
 };
 
-const signup = (request, response) => {
-  const req = request;
-  const res = response;
+const signup = (request,response) => {
+    const req = request;
+    const res = response;
 
-  req.body.username = `${req.body.username}`;
-  req.body.pass = `${req.body.pass}`;
-  req.body.pass2 = `${req.body.pass2}`;
+    req.body.username = `${req.body.username}`;
+    req.body.pass = `${req.body.pass}`;
+    req.body.pass2 = `${req.body.pass2}`;
 
-  if (!req.body.username || !req.body.pass || !req.body.pass2) {
-    return res.status(400).json({ error: 'RAWR! All fields are required' });
-  }
+    if(!req.body.username || !req.body.pass || !req.body.pass2) {
+        return res.status(400).json({error:'RAWR! All fields are required'});
+    }
 
-  if (req.body.pass !== req.body.pass2) {
-    return res.status(400).json({ error: 'RAWR! Passwords do not match' });
-  }
+    if(req.body.pass !== req.body.pass2){
+        return res.status(400).json({error:'RAWR! Passwords do not match'});
+    }
 
-  return Account.AccountModel.generateHash(req.body.pass, (salt, hash) => {
-    const accountData = {
-      username: req.body.username,
-      salt,
-      password: hash,
-    };
+    return Account.AccountModel.generateHash(req.body.pass,(salt,hash) => {
+        const accountData = {
+            username: req.body.username,
+            salt,
+            password: hash,
+        };
 
-    const newAccount = new Account.AccountModel(accountData);
+        const newAccount = new Account.AccountModel(accountData);
 
-    const savePromise = newAccount.save();
+        const savePromise = newAccount.save();
 
-    savePromise.then(() => {
-      req.session.account = Account.AccountModel.toAPI(newAccount);
-      res.json({ redirect: '/maker' });
+        savePromise.then(() => res.json({redirect:'/maker'}));
+
+        savePromise.catch((err) => {
+            console.log(err);
+
+            if(err.code === 11000){
+                return res.status(400).json({error:'Username already in use'});
+            }
+
+            return res.status(400).json({error: 'An error occurred'});
+        });
     });
-
-    savePromise.catch((err) => {
-      console.log(err);
-
-      if (err.code === 11000) {
-        return res.status(400).json({ error: 'Username already in use' });
-      }
-
-      return res.status(400).json({ error: 'An error occurred' });
-    });
-  });
 };
 
 module.exports.loginPage = loginPage;
